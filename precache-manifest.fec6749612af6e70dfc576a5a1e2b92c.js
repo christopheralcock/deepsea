f4e726f8250d1fdc4b1966e9190877d8d8a0bd9e
Christopher Alcock <christopher.alcock@gmail.com>
2019-12-10 11:40:11 +0000

    Updates

---
+++ precache-manifest.fec6749612af6e70dfc576a5a1e2b92c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e4000a870c80bd8b9c99f25d71ef7b",
+    "url": "/deepsea/index.html"
+  },
+  {
+    "revision": "549d2dde82ed6538d335",
+    "url": "/deepsea/static/css/main.df1ba17b.chunk.css"
+  },
+  {
+    "revision": "4b0e30f753de07dca2dc",
+    "url": "/deepsea/static/js/2.f3bb3aa7.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/deepsea/static/js/2.f3bb3aa7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "549d2dde82ed6538d335",
+    "url": "/deepsea/static/js/main.a2dd9f11.chunk.js"
+  },
+  {
+    "revision": "81ace2721e913d71f20f",
+    "url": "/deepsea/static/js/runtime-main.372a241d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deepsea/static/media/logo.5d5d9eef.svg"
+  }
+]);
